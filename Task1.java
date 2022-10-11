6e9fd453c9fd52b6ec4589ae8f0d1cced4591a2b
Gajal21 <115548474+Gajal21@users.noreply.github.com>
2022-10-11 16:48:39 +0530

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Calculations{
+  static int add(int a,int b)
+  {
+    return a+b;
+  }
+ 
+  public static void main(String args[])
+  {
+     Scanner sc=new Scanner(System.in);
+     
+     System.out.println("Enter your num1");
+     int num1=sc.nextInt();
+     System.out.println("Enter your num2");
+     int num2=sc.nextInt();
+     System.out.println("Addition: "+add(num1,num2));
+   
+  }
+  }
